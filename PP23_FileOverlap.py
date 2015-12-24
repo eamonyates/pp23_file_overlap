f1b5e216663de61f0628126b70a7f19802936985
Eamon Yates <eamon.yates@me.com>
2015-12-24 20:48:00 +0000

    Update PP23_FileOverlap.py
    Updated filenames

--- PP23_FileOverlap.py
+++ PP23_FileOverlap.py
@@ -6,24 +6,24 @@ def file_overlap(url1, url2):
 	r1 = requests.get(url1)
 	soup1 = BeautifulSoup(r1.text, 'html.parser')
 
-	with open('test1.txt', 'w') as open_file:
+	with open('fileOverlap1.txt', 'w') as open_file:
 		open_file.write(str(soup1))
 
 	r2 = requests.get(url2)
 	soup2 = str(BeautifulSoup(r2.text, 'html.parser'))
 
-	with open('test2.txt', 'w') as open_file:
+	with open('fileOverlap2.txt', 'w') as open_file:
 		open_file.write(str(soup2))
 
 	list1 = []
 	listOverlap = []
 
-	with open('test1.txt', 'r') as open_file:	
+	with open('fileOverlap1.txt', 'r') as open_file:	
 		for line in open_file:
 			number = line.strip()
 			list1.append(int(number))
 
-		with open('test2.txt', 'r') as open_file:	
+		with open('fileOverlap2.txt', 'r') as open_file:	
 			for line in open_file:
 				number = int(line.strip())
 				if number in list1:				
